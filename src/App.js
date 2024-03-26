ced53949c2525ead564d65dbc0c9e0a53001ba6c
Raj Makadia <raj.makadia@fuzzycloud.in>
2024-03-27 03:26:57 +0530

    task_three

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Examples:
+// sum(["1", "five", "2wenty", "thr33"]) ➞ 36
+
+// sum([["1X2", "t3n"],["1024", "5", "64"]]) ➞ 1099
+
+// sum([[["1"], "10v3"], ["738h"], [["s0"], ["1mu4ch3"],"-1s0"]]) ➞ 759
+
+// Notes:
+// - Numbers in strings can be negative, but will all be base-10 integers.
+// - Negative numbers may directly follow another number.
+// - The hyphen or minus character ("-") does not only occur in numbers.
+// - Arrays may be ragged or empty.
+
+function sum(arr) {
+
+  let total = 0;
+
+  function isValidNumber(str) {
+    return !isNaN(Number(str))
+  }
+
+  function traverse(arr) {
+    arr.forEach(item => {
+      if (Array.isArray(item)) {
+        traverse(item)
+      } else {
+        const number = item.split(/\D+/).filter(isValidNumber)
+
+        total += number.reduce((acc, num) => acc + Number(num), 0)
+      }
+    })
+  }
+  traverse(arr);
+  return total
+}
+
 function App() {
+
+  const result = sum([
+    (["1", "five", "2wenty", "thr33"])
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Result:{result}</h2>
     </div>
   );
 }
